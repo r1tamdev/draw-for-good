d192c127e5f80bf3809233efe1bcf965e3350296
Ritam Chowdhury <ritamchowdhury404@gmail.com>
2026-09-23 00:12:37 +0530

    fixed

--- server/server.js
+++ server/server.js
@@ -3,6 +3,6 @@ import app from './src/app.js';
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
+app.listen(PORT,'0.0.0.0',()=>{
   console.log(`Server is running on port ${PORT}`);
 });
